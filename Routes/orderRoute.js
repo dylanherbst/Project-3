2c5d943f93d9ad533bb46e76e0fec13ce59cebef
dylanherbst <dylan.herbstwork@gmail.com>
2024-03-22 08:27:58 +0800

    Back End

--- Routes/orderRoute.js
+++ Routes/orderRoute.js
@@ -1,29 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const Controllers = require("../Controllers");
-// matches GET requests sent to /api/users
-// (the prefix from server.js)
 
-router.get('/', (req, res) => {
-Controllers.orderController.getOrder(res);
-})
-// matches POST requests sent to /api/users/create
-// router.post('/create', (req, res) => {
-// Controllers.orderController.createOrder(req.body, res)
-// })
-router.post('/create', (req, res) => {
-    Controllers.orderController.createOrder(req, res);
+router.get("/", (req, res) => {
+  Controllers.orderController.getOrder(res);
 });
 
-// matches PUT requests to /api/users/123 (stores 123 in id param)
-router.put('/:id', (req, res) => {
-Controllers.orderController.updateOrder(req, res)
-})
+router.post("/create", (req, res) => {
+  Controllers.orderController.createOrder(req, res);
+});
 
-// matches DELETE requests to /api/users/123 (123 in id param)
-router.delete('/:id', (req, res) => {
-Controllers.orderController.deleteOrder(req, res)
-})
+router.put("/:id", (req, res) => {
+  Controllers.orderController.updateOrder(req, res);
+});
 
+router.delete("/:id", (req, res) => {
+  Controllers.orderController.deleteOrder(req, res);
+});
 
 module.exports = router;
--- dbConnect.js
+++ dbConnect.js
@@ -1,24 +1,66 @@
-'use strict';
-require('dotenv').config();
-const { Sequelize } = require('sequelize');
+"use strict";
+require("dotenv").config();
+const { Sequelize } = require("sequelize");
 // Sequelize is a package that abstracts out the need to write
 // SQL queries, relying instead on their models to do it for you
-const sequelize = new Sequelize(process.env.DB_NAME,
-process.env.DB_USER, process.env.DB_PASSWORD, {
-host: process.env.DB_HOST,
-dialect: 'mysql'
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  }
+);
 const connectMysql = async () => {
-try {
-await sequelize.authenticate();
-console.log(`Successful connection to MySQL Database
+  try {
+    await sequelize.authenticate();
+    console.log(`Successful connection to MySQL Database
 ${process.env.DB_NAME}`);
-} catch (error) {
-console.error('Unable to connect to MySQL database:', error);
-process.exit(1);
-}
-}
+  } catch (error) {
+    console.error("Unable to connect to MySQL database:", error);
+    process.exit(1);
+  }
+};
 connectMysql();
 module.exports = {
-Sequelize: sequelize
-}
+  Sequelize: sequelize,
+};
+
+// ///////// API TEST CODE //////////////
+// "use strict";
+// require("dotenv").config();
+// const { Sequelize } = require("sequelize");
+
+// // Specify charset and collation options in Sequelize connection
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     dialect: "mysql",
+//     dialectOptions: {
+//       charset: "utf8mb4",
+//       collate: "utf8mb4_unicode_ci",
+//     },
+//   }
+// );
+
+// const connectMysql = async () => {
+//   try {
+//     await sequelize.authenticate();
+//     console.log(
+//       `Successful connection to MySQL Database ${process.env.DB_NAME}`
+//     );
+//   } catch (error) {
+//     console.error("Unable to connect to MySQL database:", error);
+//     process.exit(1);
+//   }
+// };
+
+// connectMysql();
+
+// module.exports = {
+//   Sequelize: sequelize,
+// };
